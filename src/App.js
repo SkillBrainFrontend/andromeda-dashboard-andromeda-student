2a2314535449ac055fe14b482774becc3eb20d5a
pirvanrobert <pirvanrobert@gmail.com>
2022-08-19 19:26:59 +0300

    Robert-card

--- src/App.js
+++ src/App.js
@@ -17,6 +17,19 @@ function App() {
       avatar: require("./images/sapovalov_alexandru.jpg"),
     },
     {
+      name: "Robert Pirvan",
+      skillBrainPosition: "Future Junior Developer",
+      actualJob: "Engineer",
+      shortDesctioption:
+        "Junior Front-End Developer and Tech enthusiast with little experience building Web and cross-platform solutions in diverse areas of industry. Tech never stay the same. Human evolve non-stop witch mean tech time fly and continue to impress. Passionate about developing - the best reward I find doing my job. No pain - No Gain. I love chess and football.",
+
+      socialMediaLiks: {
+        gitHub: "https://github.com/pirvanrobert",
+        linkedIn: "https://www.linkedin.com/in/pirvan-robert-07655a1b0/",
+      },
+      avatar: require("./images/Robert.jpg"),
+    },
+    {
       name: "Eladie Bumbar",
       skillBrainPosition: "Student",
       actualJob: "Frontend Developer",
@@ -63,19 +76,6 @@ function App() {
         linkedIn: "https://www.linkedin.com/in/daniel-neagoe-601709223/",
       },
       avatar: require("./images/daniel_neagoe.jpg"),
-    },
-    {
-      name: "Robert Pirvan",
-      skillBrainPosition: "Future Junior Developer",
-      actualJob: "Engineer",
-      shortDesctioption:
-        "Junior Front-End Developer and Tech enthusiast with little experience building Web and cross-platform solutions in diverse areas of industry. Tech never stay the same. Human evolve non-stop witch mean tech time fly and continue to impress. Passionate about developing - the best reward I find doing my job. No pain - No Gain. I love chess and football.",
-
-      socialMediaLiks: {
-        gitHub: "https://github.com/pirvanrobert",
-        linkedIn: "https://www.linkedin.com/in/pirvan-robert-07655a1b0/",
-      },
-      avatar: require("./images/Robert.jpg"),
     },
   ];
 
